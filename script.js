aedbbc89a803adde87614bc4662f82c3a6156033
emiaraujo11 <158339127+emiaraujo11@users.noreply.github.com>
2024-12-09 14:51:51 -0300

    Add projeto

---
+++ script.js
@@ -0,0 +1,9 @@
+function redirecionar(){
+    window.location.href = "pagina2.html";
+}
+
+
+document.getElementById("voltar").addEventListener("click", function() {
+    window.location.href = "index.html"; 
+});
+
